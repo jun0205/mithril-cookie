eaa45ce168b10b6d8450d185934562a5903159e5
John <jun0205@foxmail.com>
2017-03-29 19:10:08 +0800

    Update mithril-cookie.js

--- mithril-cookie.js
+++ mithril-cookie.js
@@ -6,8 +6,6 @@
 
   window.mx = window.mx || {};
 
-  var pluses = /\+/g;
-
   function encode(s) {
     return encodeURIComponent(s).replace(/%(23|24|26|2B|3A|3C|3E|3D|2F|3F|40|5B|5D|5E|60|7B|7D|7C)/g, decodeURIComponent);
   }
